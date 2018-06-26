66ce1fa49cac4a5cba5e530107fb5f50016bd5ae
Alberico <alberico.zitiello@outlook.it>
2018-06-26 15:10:33 +0200

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Widget from './components/Widget'
 import Grafico from './components/Grafico'
+import Logo from './components/Logo'
 import './css/style.css'
 
 class App extends Component {
   render() {
     return (
       <Widget title="Prova">
+        <Logo />
         <Grafico />
       </Widget>
     )
