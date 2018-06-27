b062b0643c98f0168d63ad514d72e34900d799d1
Alberico <alberico.zitiello@outlook.it>
2018-06-27 11:34:04 +0200

    logo in header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import Main from './components/Main'
 import Grafico from './components/Grafico'
-import Logo from './components/Logo'
 import './css/style.css'
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
+import Logo from './components/Logo'
 
 class App extends Component {
   render() {
