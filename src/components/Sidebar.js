dbe1e56aa6dd853ddc97a296cfc7b4eb781a69c4
Marta <martabassi111@gmail.com>
2018-06-28 18:43:09 +0200

    sidebar e header da rivedere

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,22 +11,18 @@ class Sidebar extends Component {
             <Logo />
           </NavLink>
         </div>
+        <hr className="hr__logo" />
         <div className="sidebar_user">
           {/* <div className="fotoUser">{this.props.username[0]}</div>
          <h2>{this.props.username}</h2> */}
           <div className="logoLogin">
-            <img className="stretch" src={require(
-              '../img/logoLogin.png')}
-            />
+            <img className="stretch" src={require('../img/logoLogin.png')} />
             <NavLink to="/login">
-              <span className="userProp">
-                Nome Utente
-              </span>
+              <span className="userProp">Nome Utente</span>
             </NavLink>
           </div>
-        </div >
-
-        <hr />
+        </div>
+        <hr className="hr__sidebar" />
         <ul>
           <NavLink className="LinkSide" activeClassName="active" to="/hashtags">
             <li>HASHTAGS</li>
@@ -49,7 +45,7 @@ class Sidebar extends Component {
             <li>COMPETITORS</li>
           </NavLink>
         </ul>
-      </div >
+      </div>
     )
   }
 }
