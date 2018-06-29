750f6f4d491c03b44c816c5a5bd13ff23a3fe287
Alberico <alberico.zitiello@outlook.it>
2018-06-29 12:55:38 +0200

    changed css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import Logo from './Logo'
 
 class Header extends Component {
@@ -19,4 +19,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default withRouter(Header)
