f8da5aa3ecfc15b15af4b8d034cee5d4019c5777
margotsodi <margot.sophia.sodi@gmail.com>
2018-07-02 10:27:45 +0200

    Widget grafici

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,13 @@ import NotFound from './NotFound.js'
 import Grafico from './Grafico'
 import Widget from './Widget.js'
 import Home_Piechart from './Home__PieChart'
+import AccountActivity from './AccountActivity.js';
+import AccountInvolvement from './AccountInvolvement.js';
+import Personality_AccountActivity from './Personality_AccountActivity.js';
+import Personality_AccountInvolvement from './Personality_AccountInvolvement.js';
+import Personality_AccountApproval from './Personality_AccountApproval.js';
+import Competitor_AccountActivity from './Competitor_AccountActivity.js';
+import Competitor_AccountInvolvement from './Competitor_AccountInvolvement.js';
 
 class Main extends Component {
   render() {
@@ -46,11 +53,98 @@ class Main extends Component {
 
           <Route path="/hashtags" render={props => <Hashtags />} />
 
-          <Route path="/accounts" render={props => <Accounts />} />
+          <Route path="/accounts" render={props => (
+            <div className="container_account_element">
+              <Widget width="90%">
+                <AccountActivity
 
-          <Route path="/personalita" render={props => <Personalita />} />
+                  title={'ACCOUNT ACTIVITY'}
+                  version="155"
+                  category="ma"
+                  subCategory="trend"
+                  social="twitter"
+                  indicator="activity"
+                />
+              </Widget>
+              <Widget width="90%">
+                <AccountInvolvement
+                  title={'ACCOUNT INVOLVEMENT'}
+                  version="158"
+                  category="ma"
+                  subCategory="trend"
+                  social="twitter"
+                  indicator="involvement"
+                />
+              </Widget>
+            </div>
 
-          <Route path="/competitors" render={props => <Competitors />} />
+          )} />
+
+          <Route path="/personalita" render={props => (
+            <div className="container_personality_element">
+              <Widget width="90%">
+                <Personality_AccountActivity
+                  title={'PERSONALITY ACTIVITY'}
+                  version="V170"
+                  category="ma"
+                  subCategory="trend"
+                  social="twitter"
+                  indicator="activity"
+                />
+              </Widget>
+              <Widget width="90%">
+                <Personality_AccountInvolvement
+                  title={'PERSONALITY INVOLVEMENT'}
+                  version="V160"
+                  category="ma"
+                  subCategory="trend"
+                  social="twitter"
+                  indicator="involvement"
+                />
+              </Widget>
+              <Widget width="90%">
+                <Personality_AccountApproval
+                  title={'PERSONALITY APPROVAL'}
+                  version="V160"
+                  category="ma"
+                  subCategory="trend"
+                  social="twitter"
+                  indicator="approval"
+                />
+              </Widget>
+            </div>
+
+          )} />
+
+
+
+          <Route path="/competitors" render={props => (
+            <div className="container_competitor_element">
+              <Widget width="90%">
+                <Competitor_AccountActivity
+                  title={'COMPETITOR ACTIVITY'}
+                  version="V158"
+                  category="ma"
+                  subCategory="trend"
+                  social="twitter"
+                  indicator="activity"
+                />
+              </Widget>
+              <Widget width="90%">
+                <Competitor_AccountInvolvement
+                  title={'COMPETITOR INVOLVEMENT'}
+                  version="V158"
+                  category="ma"
+                  subCategory="trend"
+                  social="twitter"
+                  indicator="involvement"
+                />
+              </Widget>
+            </div>
+
+          )} />
+
+
           <Route render={props => <NotFound />} />
         </Switch>
       </div>
