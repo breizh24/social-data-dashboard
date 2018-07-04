edb75dfd91ae9ea5ce60c0ade135ec5307bbd47d
Alberico <alberico.zitiello@outlook.it>
2018-07-04 12:29:46 +0200

    checkbox for linechart

--- src/components/Personality_AccountApproval.js
+++ src/components/Personality_AccountApproval.js
@@ -11,6 +11,8 @@ import {
   Legend,
 } from 'recharts'
 import moment from 'moment'
+import Widget from './Widget'
+import { Checkbox } from 'primereact/components/checkbox/Checkbox'
 
 class Personality_AccountApproval extends Component {
   constructor(props) {
@@ -18,23 +20,52 @@ class Personality_AccountApproval extends Component {
     this.state = {
       apiData: [],
     }
+    this.color = [
+      'red',
+      'yellow',
+      'green',
+      'blue',
+      '#ccc',
+      '#585858',
+      '#d786e3',
+      '#c6004a',
+      '#7480b5',
+      '#75b586',
+    ]
   }
 
   componentDidMount() {
-    fetch('http://165.227.158.131/dp/api/v160/trend/ma/twitter/order/approval')
+    fetch(
+      'http://165.227.158.131/dp/api/v160/trend/ma/twitter/range/2018-05-01/2018-05-20/order/approval',
+    )
       .then(response => response.json())
       .then(response => {
         let apiData = response.apiData.data
         this.setState({
           apiData: apiData,
         })
-        console.log(this.state.apiData)
       })
   }
 
-  handleApiData = data => {
-    let myData = data.slice()
+  onChangeCheckbox = (idx, data) => {
+    data[idx].checked = !data[idx].checked
+    console.log(data)
+  }
 
+  handleApiData = data => {
+    let myData = data.map((el, idx) => {
+      const newObj = el.days.map(element => {
+        const obj = { ...element }
+        return obj
+      })
+      const result = {
+        ...el,
+        days: newObj,
+        color: this.color[idx],
+        checked: true,
+      }
+      return result
+    })
     for (let i = 0; i < myData[0].days.length; i++) {
       let dayCompare = myData[0].days[i].day
       for (let j = 0; j < myData.length; j++) {
@@ -45,20 +76,31 @@ class Personality_AccountApproval extends Component {
             )
             break
           } else {
-            if (myData[j].days.find(el => el.day === dayCompare) === undefined)
+            if (
+              myData[j].days.find(el => el.day === dayCompare) === undefined
+            ) {
               myData[j].days.push({
                 day: dayCompare,
                 value: 0,
               })
+            }
           }
         }
       }
     }
+    console.log(myData)
     return myData
   }
 
   handleDataLineChart = apiData => {
-    let data = apiData.slice()
+    let data = apiData.map(el => {
+      const newObj = el.days.map(element => {
+        const obj = { ...element }
+        return obj
+      })
+      const result = { ...el, days: newObj }
+      return result
+    })
     let dataLineChart = []
     let tempDay
     data.map(obj => {
@@ -81,7 +123,7 @@ class Personality_AccountApproval extends Component {
         }
       })
     })
-    // console.log(dataLineChart)
+    console.log(dataLineChart)
     return dataLineChart
   }
 
@@ -94,43 +136,51 @@ class Personality_AccountApproval extends Component {
   }
 
   render() {
-    let color = [
-      'red',
-      'yellow',
-      'green',
-      'blue',
-      '#ccc',
-      '#585858',
-      '#d786e3',
-      '#c6004a',
-      '#7480b5',
-      '#75b586',
-    ]
     let dataLineChart = []
+    let data = []
     if (this.state.apiData.length > 0) {
-      let data = this.handleApiData(this.state.apiData)
+      data = this.handleApiData(this.state.apiData)
       dataLineChart = this.handleTime(data)
     }
+
     return (
-      <div>
-        <ResponsiveContainer width="100%" height={500}>
-          <LineChart data={dataLineChart}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="day" />
-            <YAxis />
-            {this.state.apiData.map((obj, idx) => (
-              <Line
-                key={idx}
-                type="monotone"
-                dataKey={`${obj.entity}`}
-                stroke={`${color[idx]}`}
-              />
-            ))}
-            <Tooltip />
-            <Legend />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+      <Widget width="100%">
+        <div className="linechart__container">
+          <ResponsiveContainer width="100%" height={600}>
+            <LineChart data={dataLineChart}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="day" />
+              <YAxis />
+              {data.map((obj, idx) => (
+                <Line
+                  key={idx}
+                  type="monotone"
+                  dataKey={`${obj.entity}`}
+                  stroke={`${obj.color}`}
+                />
+              ))}
+              <Tooltip />
+            </LineChart>
+          </ResponsiveContainer>
+          <div className="linechart__legend">
+            <ul className="linechart__legend__ul">
+              {data.map((obj, idx) => (
+                <li key={idx}>
+                  <span>
+                    <input
+                      type="checkbox"
+                      data-id={idx}
+                      defaultChecked={data[idx].checked}
+                      onChange={() => this.onChangeCheckbox(idx, data)}
+                    />
+                  </span>
+                  <span>{obj.entity}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Widget>
     )
   }
 }
