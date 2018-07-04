bde7a86c778ab17c1811143b35cbe56640126cbc
VynsD <v.dellavv@gmail.com>
2018-07-04 15:23:25 +0200

    NetWork Graph

--- src/components/Hashtags.js
+++ src/components/Hashtags.js
@@ -8,6 +8,7 @@ import {
   ResponsiveContainer,
 } from 'recharts'
 import Widget from './Widget.js'
+import HashNetwork from "./HashNetwork"
 import HashtagPos_CustomPieChart from './HashtagPos_CustomPieChart'
 import HashtagNeg_CustomPieChart from './HashtagNeg_CustomPieChart'
 
@@ -17,6 +18,7 @@ class Hashtags extends Component {
   render() {
     return (
       <div className="container__home__element">
+        <HashNetwork />
         <Widget width="45%">
           <HashtagPos_CustomPieChart
             title={'SENTIMENT - POSITIVITY'}
